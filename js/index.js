3c89bdc79ded47608ae62a500a6afa6d3b696237
Shinkai01 <133118652+Shinkai01@users.noreply.github.com>
2025-02-08 16:20:10 +0800

    push

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let welcomeText = document.getElementById("welcome-text");
+    welcomeText.style.color = "#ff5733"; // Change text color dynamically
+});
