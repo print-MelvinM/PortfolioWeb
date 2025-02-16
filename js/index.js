412cf10a3316b23d0d3cdc5b7df290aa7a687839
Shinkai01 <133118652+Shinkai01@users.noreply.github.com>
2025-02-16 12:10:32 +0800

    push

--- js/index.js
+++ js/index.js
@@ -3,7 +3,22 @@ document.addEventListener("DOMContentLoaded", function() {
         let welcomeText = document.getElementById("welcome-text");
         welcomeText.classList.add("fade-out");
         setTimeout(() => {
-            window.location.href = "front.html";
+            window.location.href = "details.html";
         }, 2000); 
     }, 5000);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const welcomeText = document.getElementById('welcome-text');
+
+    // Trigger fadeOut after fadeIn
+    setTimeout(function() {
+        welcomeText.classList.add('fade-out');
+    }, 3000); // Adjust the timeout as needed
+
+    welcomeText.addEventListener('animationend', function(event) {
+        if (event.animationName === 'fadeOut') {
+            document.body.classList.add('black-background');
+        }
+    });
 });
